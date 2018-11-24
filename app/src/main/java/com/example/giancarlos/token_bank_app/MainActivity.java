7c4539e2f87cce9c42bb90fbd6e96baa1152e8fe
Carlos Eli <carloselilopezt@gmail.com>
2018-11-23 23:16:53 -0600

    Token generation by algorithm
    Simple algorithm to take 5 digit number from current time

--- app/src/main/java/com/example/giancarlos/token_bank_app/MainActivity.java
+++ app/src/main/java/com/example/giancarlos/token_bank_app/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.giancarlos.token_bank_app;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -14,16 +16,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Handler h = new Handler();
+        h.post(new Runnable() {
+            @Override
+            public void run() {
+                String generatedPassword = String.valueOf(System.currentTimeMillis());
+                generatedPassword =
+                        String.valueOf(generatedPassword.charAt(0)) +
+                        String.valueOf(generatedPassword.charAt(2)) +
+                        String.valueOf(generatedPassword.charAt(4)) +
+                        String.valueOf(generatedPassword.charAt(6)) +
+                        String.valueOf(generatedPassword.charAt(8));
+
+                TextView token = (TextView)findViewById(R.id.token);
+                String tokenString = String.valueOf(generatedPassword);
+                token.setText(tokenString);
+
+                h.postDelayed(this,
+                        DateUtils.MINUTE_IN_MILLIS - System.currentTimeMillis() % DateUtils.MINUTE_IN_MILLIS);
+            }
+        });
     }
 
-    public void generate (View view){
-
-
-        Random random = new Random();
-        String generatedPassword = String.format("%04d0", random.nextInt(10000));
-
-        TextView token = (TextView)findViewById(R.id.token);
-        String tokenString = String.valueOf(generatedPassword);
-        token.setText(tokenString);
-    }
 }
